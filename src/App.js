8dd3e2c9ac8e0709657414ace80cb4f34171f458
Nitin Khodade <nitinkhodade025@gmail.com>
2024-09-03 13:49:33 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,1 +1,21 @@
 
+import React from 'react';
+import './App.css';
+import Header from './components/Header';
+import Nav from './components/Nav';
+import Footer from './components/footer';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <div className='main-container'>
+        <Nav />
+        <Footer />
+      </div>
+    </div>
+  );
+}
+
+export default App;
